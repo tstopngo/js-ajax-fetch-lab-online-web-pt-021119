c5551850ecd885f9b9c667f7ea13563099c92265
Tracy Ngo <tngo1206@gmail.com>
2019-09-16 20:30:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const user = 'tstopngo';
+
+
 
 function getToken() {
   //change to your token to run in browser, but set
@@ -9,17 +11,69 @@ function getToken() {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  const url = `${baseURL}/repos/${repo}/forks`;
   //use fetch to fork it!
+
+  fetch(url,{
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(resp => resp.json())
+    .then(json => showResults(json));
 }
 
-function showResults(json) {
+function showResults(repo) {
   //use this function to display the results from forking via the API
+  document.getElementById('results').innerHTML = `<a href=${repo.html_url}>
+  ${repo.html_url}</a>`;
 }
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const repo = 'js-ajax-fetch-lab'
+  const url = `${baseURL}/repos/${user}/${repo}/issues`
+  const postData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  };
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(resp => resp.json())
+    .then(json => getIssues());
 }
 
-function getIssues() {
+function getIssues(){
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const repo = 'js-ajax-fetch-lab'
+  const url = `${baseURL}/repos/${user}/${repo}/issues`
+
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+  .then(resp => resp.json())
+  .then(json => listIssues(json));
+}
+
+function listIssues(issues){
+  const issueList = `<ul>
+                        ${issues.map(issue =>
+                          '<li>'+
+                          issue.title +
+                          ' - '  +
+                          issue.body +
+                          '</li>').join('')
+                          }
+                    </ul>`;
+
+  document.getElementById('issues').innerHTML = issueList
 }
